fa234d7e33326e84b62e5f9d0f447b4f1f4b6244
bistadilraj-star <bista.dilraj@gmail.com>
2025-11-16 03:59:31 +0000

    Keep only week3 folder in Week3 branch

---
+++ Dcs403-assignmnet-backup/week3/day1/Q8.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int r;
+    cout << "Enter number of rows you want to make for floyd's traiangle : ";
+    cin >> r;
+
+    int num = 1;
+    for (int i = 1; i <= r; i++)
+    {
+    
+            for (int j = 1; j <= i; j++)
+        {
+            cout << num << " ";
+            num++;
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
